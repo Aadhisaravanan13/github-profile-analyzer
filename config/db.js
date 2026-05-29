bb1208820e396006d2d1505232e0558f7cf09cbf
Aadhi Saravanan <mr.aadhi2005@gmail.com>
2026-05-29 13:55:31 +0530

    DB connection fix

--- config/db.js
+++ config/db.js
@@ -1,22 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     ssl: {
         rejectUnauthorized: false,
     },
-});
-
-connection.connect((err) => {
-    if(err){
-        console.log("❌ Database connection failed", err);
-    } else {
-        console.log("✅ MySql Connected Successfully");
-    }
 });
 
 module.exports = connection;
